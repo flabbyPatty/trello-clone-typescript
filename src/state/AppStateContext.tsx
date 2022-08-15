78095f1eec1f8e2471015704709070b94d19bd04
Patrick Santos <pgsantos.92@gmail.com>
2022-08-16 06:57:40 +0800

    Implement global state

---
+++ src/state/AppStateContext.tsx
@@ -0,0 +1,36 @@
+import { createContext, useContext, FC } from "react";
+
+type Task = {
+  id: string;
+  text: string;
+};
+
+type List = {
+  id: string;
+  text: string;
+  tasks: Task[];
+};
+
+export type AppState = {
+  lists: List[];
+};
+
+const appData: AppState = {
+  lists: [
+    {
+      id: "0",
+      text: "To Do",
+      tasks: [{ id: "c0", text: "Generate app scaffold" }],
+    },
+    {
+      id: "1",
+      text: "In Progress",
+      tasks: [{ id: "c2", text: "Learn TypeScript" }],
+    },
+    {
+      id: "2",
+      text: "Done",
+      tasks: [{ id: "c3", text: "Begin to use static typing" }],
+    },
+  ],
+};
