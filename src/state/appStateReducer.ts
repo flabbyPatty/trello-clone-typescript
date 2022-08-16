e288c06c37357f9a73c6cc935bd5085600b4b615
Patrick Santos <pgsantos.92@gmail.com>
2022-08-16 08:16:32 +0800

    Implement adding tasks

--- src/state/appStateReducer.ts
+++ src/state/appStateReducer.ts
@@ -1,5 +1,6 @@
 import { nanoid } from "nanoid";
 import { Action } from "./actions";
+import { findItemIndexById } from "../utils/arrayUtils";
 
 export type Task = {
   id: string;
@@ -28,6 +29,14 @@ export const appStateReducer = (
         tasks: [],
       });
       break;
+    }
+    case "ADD_TASK": {
+      const { text, listId } = action.payload;
+      const targetListIndex = findItemIndexById(draft.lists, listId);
+      draft.lists[targetListIndex].tasks.push({
+        id: nanoid(),
+        text,
+      });
     }
   }
 };
