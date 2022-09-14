fb794044d28ca772ff032a08b649ec84e89cc375
Patrick Santos <pgsantos.92@gmail.com>
2022-09-14 11:55:25 +0800

    Implement moving of items and lists

--- src/state/appStateReducer.ts
+++ src/state/appStateReducer.ts
@@ -1,21 +1,21 @@
-import { nanoid } from "nanoid";
-import { Action } from "./actions";
-import { findItemIndexById } from "../utils/arrayUtils";
+import { nanoid } from "nanoid"
+import { Action } from "./actions"
+import { findItemIndexById, moveItem } from "../utils/arrayUtils"
 
 export type Task = {
-  id: string;
-  text: string;
-};
+  id: string
+  text: string
+}
 
 export type List = {
-  id: string;
-  text: string;
-  tasks: Task[];
-};
+  id: string
+  text: string
+  tasks: Task[]
+}
 
 export type AppState = {
-  lists: List[];
-};
+  lists: List[]
+}
 
 export const appStateReducer = (
   draft: AppState,
@@ -27,16 +27,24 @@ export const appStateReducer = (
         id: nanoid(),
         text: action.payload,
         tasks: [],
-      });
-      break;
+      })
+      break
     }
     case "ADD_TASK": {
-      const { text, listId } = action.payload;
-      const targetListIndex = findItemIndexById(draft.lists, listId);
+      const { text, listId } = action.payload
+      const targetListIndex = findItemIndexById(draft.lists, listId)
       draft.lists[targetListIndex].tasks.push({
         id: nanoid(),
         text,
-      });
+      })
+      break
+    }
+    case "MOVE_LIST": {
+      const { draggedId, hoverId } = action.payload
+      const dragIndex = findItemIndexById(draft.lists, draggedId)
+      const hoverIndex = findItemIndexById(draft.lists, hoverId)
+      draft.lists = moveItem(draft.lists, dragIndex, hoverIndex)
+      break
     }
   }
-};
+}
